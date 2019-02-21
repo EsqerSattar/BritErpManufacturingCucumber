02e1cc33ff93cd1d65d80a6146bcfd8fa5f67c60
Esqer Sattar <bireylen@gmail.com>
2019-02-21 00:21:59 -0700

    some method been added

--- src/test/java/com/britErp/runners/SmokeTestRunner.java
+++ src/test/java/com/britErp/runners/SmokeTestRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
             features = "src/test/resources/features",
             glue = "com/britErp/step_definitions"
             , dryRun = false
-            , tags = "@test1"
+            , tags = "@smokeTest"
     )
     public class SmokeTestRunner {
 
